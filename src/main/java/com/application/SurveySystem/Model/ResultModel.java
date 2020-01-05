35fc95a43e508df342a1ad33145d536903650cc7
Harshvardhan Ojha <42267022+hv-ojha@users.noreply.github.com>
2020-01-05 21:55:37 +0530

    add ResultModel.java

---
+++ src/main/java/com/application/SurveySystem/Model/ResultModel.java
@@ -0,0 +1,13 @@
+package com.application.SurveySystem.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResultModel {
+    private Integer id;
+    private boolean answer;
+}
